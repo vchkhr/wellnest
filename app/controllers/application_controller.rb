9b621e368b929e4eac219a159460a2b07d308c24
Viacheslav Kharchenko <vchkhr@gmail.com>
2021-12-21 13:25:39 +0200

    Add Dashboard Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  private
+  def authenticate_user!
+    if user_signed_in?
+      redirect_to dashboard
+    else
+      redirect_to home_path
+    end
+  end
 end
