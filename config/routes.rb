d361021d62ed96c158fd9dcd4a302f39cf698f9f
Viacheslav Kharchenko <vchkhr@gmail.com>
2021-12-30 23:50:00 +0200

    Add styles to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   root 'home#index'
   
+  # Auth
   resources :users
-
   resources :sessions, only: [:new, :create, :destroy]
-
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
-
   resources :password_resets
+
+  # App
 end
+
+
