a275d9a38d412d759d0ec83b3c08870d7af11929
Viacheslav Kharchenko <vchkhr@gmail.com>
2022-01-03 16:41:58 +0200

    Add Active Admin

---
+++ app/admin/users.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register User do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :email, :password_digest, :is_verified, :send_email_notifications, :password_reset_token, :password_reset_sent_at
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :email, :password_digest, :is_verified, :send_email_notifications, :password_reset_token, :password_reset_sent_at]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+AdminUser.create!(:email => 'test@test.com', :password => 'password', :password_confirmation => 'password')
+
 Gender.create(name: 'Male')
 Gender.create(name: 'Female')
 
@@ -75,3 +77,4 @@ Message.create!(client: client1, coach: coach1, from_client: false, text: 'How d
 Message.create!(client: client1, coach: coach1, from_client: true, text: 'Better now. I began to sleep better at night.')
 Message.create!(client: client1, coach: coach1, from_client: false, text: 'Excellent! Here is a link to a new technique for you: coachingplatform.com/technique/123 Begin to pass it tomorrow.')
 Message.create!(client: client1, coach: coach1, from_client: true, text: 'Ok. Thanks')
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
