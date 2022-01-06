c6387a80e1fdb517dc5d14bab67bb236b54ce66b
Viacheslav Kharchenko <vchkhr@gmail.com>
2022-01-06 18:48:11 +0200

    Try to test controllers

--- app/controllers/techniques_steps_controller.rb
+++
@@ -1,9 +0,0 @@
-class TechniquesStepsController < InheritedResources::Base
-
-  private
-
-    def techniques_step_params
-      params.require(:techniques_step).permit(:technique_id, :step_id)
-    end
-
-end
--- test/models/techniques_step_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class TechniquesStepTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
