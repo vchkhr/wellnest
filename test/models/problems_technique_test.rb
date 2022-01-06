76410c28102361fbc88342546080200a72674b0d
Viacheslav Kharchenko <vchkhr@gmail.com>
2022-01-06 18:58:51 +0200

    Remove unneeded tests

--- test/models/problems_technique_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ProblemsTechniqueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/user_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class UserTest < ActiveSupport::TestCase
-  test "should not save user without name" do
-    user = User.new(email: 'test@example.com', password: 'abc12345')
-    assert_not user.save
-  end
-end
