f9be402c37984f51c11f50ceb4b12348bb458af2
Viacheslav Kharchenko <vchkhr@gmail.com>
2022-01-07 14:10:52 +0200

    Fix Docker

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,47 @@
-FROM ruby:3.0.1
+FROM ruby:3.0.1-alpine
 
-RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
+ENV BUNDLER_VERSION=2.0.2
 
-RUN mkdir /myapp
-WORKDIR /myapp
+RUN apk add --update --no-cache \
+      binutils-gold \
+      build-base \
+      curl \
+      file \
+      g++ \
+      gcc \
+      git \
+      less \
+      libstdc++ \
+      libffi-dev \
+      libc-dev \
+      linux-headers \
+      libxml2-dev \
+      libxslt-dev \
+      libgcrypt-dev \
+      make \
+      netcat-openbsd \
+      nodejs \
+      openssl \
+      pkgconfig \
+      postgresql-dev \
+      py-pip \
+      tzdata \
+      yarn
 
-COPY Gemfile /myapp/Gemfile
-COPY Gemfile.lock /myapp/Gemfile.lock
+RUN gem install bundler -v 2.2.30
 
-RUN bundle install
+WORKDIR /app
 
-COPY . /myapp
+COPY Gemfile Gemfile.lock ./
+
+RUN bundle config build.nokogiri --use-system-libraries
+
+RUN bundle check || bundle install
+
+COPY package.json yarn.lock ./
+
+RUN yarn install --check-files
+
+COPY . ./
+
+ENTRYPOINT ["./entrypoints/docker-entrypoint.sh"]
---
+++ test/controllers/coaches_controoler_test.rb
@@ -0,0 +1,47 @@
+require "test_helper"
+
+class CoachesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    Gender.create(name: 'Male')
+    Gender.create(name: 'Female')
+
+    Problem.create(name: 'Anxiety')
+    Problem.create(name: 'Depression')
+    Problem.create(name: 'Irritability')
+    Problem.create(name: 'Stress')
+
+    @user = User.create!(name: 'James Brown', email: 'james@brown.com', password: 'abc12345')
+    @coach = Coach.create!(age: 36, user: @user, gender: Gender.find_by_name('Male'), education: 'education', work: 'work', licenses: 'licenses', links: 'http://example.org', problems: [Problem.find_by_name('Anxiety'), Problem.find_by_name('Depression')])
+  end
+
+  test "should create coach" do
+    user = User.create!(name: 'James Brown', email: 'james2@brown.com', password: 'abc12345')
+    coach = Coach.create!(age: 36, user: user, gender: Gender.find_by_name('Male'), education: 'education', work: 'work', licenses: 'licenses', links: 'http://example.org', problems: [Problem.find_by_name('Anxiety'), Problem.find_by_name('Depression')])
+
+    if coach.save
+      assert true
+    else
+      assert false
+    end
+  end
+
+  test "should get edit" do
+    @coach.save
+    get edit_coach_url(@coach)
+    assert_response :success
+  end
+
+  test "should update coach" do
+    @coach.save
+    patch coach_url(@coach), params: { coach: { work: "Updated work" } }
+    assert_redirected_to dashboard_url
+  end
+
+  test "should destroy coach" do
+    assert_difference('Coach.count', -1) do
+      delete coach_url(@coach)
+    end
+
+    assert_redirected_to root_url
+  end
+end
