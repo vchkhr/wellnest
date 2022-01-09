0928b428a4d23edcbeed469045c84ec0f5ae2566
Viacheslav Kharchenko <vchkhr@gmail.com>
2022-01-09 15:40:05 +0200

    Add ActiveAdmin

---
+++ app/admin/completed_steps.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register CompletedStep do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :client_id, :step_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:client_id, :step_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/admin/likes.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Like do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :is_like, :technique_id, :client_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:is_like, :technique_id, :client_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
