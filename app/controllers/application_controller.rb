6d3de6a780cfa94a694587be060fd4454b9adcce
Viacheslav Kharchenko <vchkhr@gmail.com>
2022-01-17 23:46:26 +0200

    Call services as helpers in views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
-  helper_method :profile_image
-  helper_method :cover_image
-  
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
@@ -10,6 +6,7 @@ class ApplicationController < ActionController::Base
       @current_user = nil
     end
   end
+  helper_method :current_user
 
   def profile_image(user)
     if user.profile.image.attached?
@@ -18,6 +15,7 @@ class ApplicationController < ActionController::Base
       "https://devshift.biz/wp-content/uploads/2017/04/profile-icon-png-898.png"
     end
   end
+  helper_method :profile_image
 
   def cover_image(technique)
     unless technique.image.nil?
@@ -26,4 +24,25 @@ class ApplicationController < ActionController::Base
       "behaviour-therapy.jpg"
     end
   end
+  helper_method :cover_image
+
+  def completed_steps_in_technique(technique)
+    ClientManager::CompletedStepsInTechnique.call(technique)
+  end
+  helper_method :completed_steps_in_technique
+
+  def techniques_completed(client)
+    ClientManager::TechniquesCompleted.call(client)
+  end
+  helper_method :techniques_completed
+
+  def techniques_in_progress(client)
+    ClientManager::TechniquesInProgress.call(client)
+  end
+  helper_method :techniques_in_progress
+
+  def techniques_not_started(client)
+    ClientManager::TechniquesNotStarted.call(client)
+  end
+  helper_method :techniques_not_started
 end
